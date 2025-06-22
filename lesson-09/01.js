286d076c2e43fb6586c6bcf76178a826cf3f1bd5
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-06-23 00:28:25 +0300

    2 коммит

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -1,14 +1,26 @@
-petShop.addEventListener('click', function (event) {
-  // Проверяем, что клик был по кнопке питомца
-  if (event.target.classList.contains('pet')) {
-    const petId = event.target.id
+const petShop = document.querySelector('.pet-shop')
+const cart = []
+const cartList = document.getElementById('cart-list')
+const messageBox = document.getElementById('message-box')
 
+function updateCartDisplay() {
+  cartList.innerHTML = ''
+  for (let petId of cart) {
+    const li = document.createElement('li')
+    li.classList.add('pet')
+    li.textContent = petId
+    cartList.append(li)
+  }
+}
+
+petShop.addEventListener('click', function (event) {
+  if (event.target.classList.contains('pet')) {
     if (cart.length >= 3) {
       messageBox.textContent = 'Вы не можете добавить более 3 питомцев'
       return
     }
 
-    cart.push(petId)
+    cart.push(event.target.id)
     messageBox.textContent = ''
     updateCartDisplay()
   }
