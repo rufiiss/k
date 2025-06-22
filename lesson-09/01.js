a56a24d28a440758cd96fe9acfd70898fdbcea62
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-06-23 00:36:56 +0300

    ком1

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -1,27 +1,29 @@
-const petShop = document.querySelector('.pet-shop')
-const cart = []
-const cartList = document.getElementById('cart-list')
-const messageBox = document.getElementById('message-box')
+document.addEventListener('DOMContentLoaded', function () {
+  const petShop = document.querySelector('.pet-shop')
+  const cart = []
+  const cartList = document.getElementById('cart-list')
+  const messageBox = document.getElementById('message-box')
 
-function updateCartDisplay() {
-  cartList.innerHTML = ''
-  for (let petId of cart) {
-    const li = document.createElement('li')
-    li.classList.add('pet')
-    li.textContent = petId
-    cartList.append(li)
-  }
-}
-
-petShop.addEventListener('click', function (event) {
-  if (event.target.classList.contains('pet')) {
-    if (cart.length >= 3) {
-      messageBox.textContent = 'Вы не можете добавить более 3 питомцев'
-      return
+  function updateCartDisplay() {
+    cartList.innerHTML = ''
+    for (let petId of cart) {
+      const li = document.createElement('li')
+      li.classList.add('pet')
+      li.textContent = petId
+      cartList.append(li)
     }
-
-    cart.push(event.target.id)
-    messageBox.textContent = ''
-    updateCartDisplay()
   }
+
+  petShop.addEventListener('click', function (event) {
+    if (event.target.classList.contains('pet')) {
+      if (cart.length >= 3) {
+        messageBox.textContent = 'Вы не можете добавить более 3 питомцев'
+        return
+      }
+
+      cart.push(event.target.id)
+      messageBox.textContent = ''
+      updateCartDisplay()
+    }
+  })
 })
