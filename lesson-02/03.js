17387c59bbcbb553189c0d0112282dae20758dca
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-06-23 01:07:50 +0300

    ком3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,15 +1,15 @@
-let grade
+;let grade;
 
-if (score >= 0 && score <= 49) {
-  grade = 'F'
-} else if (score <= 69) {
-  grade = 'D'
-} else if (score <= 79) {
-  grade = 'C'
-} else if (score <= 89) {
-  grade = 'B'
-} else if (score <= 100) {
-  grade = 'A'
+if (score >= 90 && score <= 100) {
+  grade = 'A';
+} else if (score >= 80 && score <= 89) {
+  grade = 'B';
+} else if (score >= 70 && score <= 79) {
+  grade = 'C';
+} else if (score >= 50 && score <= 69) {
+  grade = 'D';
+} else if (score >= 0 && score <= 49) {
+  grade = 'F';
 } else {
-  grade = 'Invalid score'
+  grade = 'Invalid score';
 }
