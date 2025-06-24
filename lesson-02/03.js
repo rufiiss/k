5fd1755bfbf4f519f4a920a85d13363b5f0ebb56
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-06-24 23:05:02 +0300

    k10

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,19 +1,16 @@
-function calculateGrade(score) {
-  let grade;
+let score = 0; // Инициализация переменной (значение будет перезаписано тестами)
+let grade;
 
-  if (score >= 90 && score <= 100) {
-    grade = 'A';
-  } else if (score >= 80 && score <= 89) {
-    grade = 'B';
-  } else if (score >= 70 && score <= 79) {
-    grade = 'C';
-  } else if (score >= 50 && score <= 69) {
-    grade = 'D';
-  } else if (score >= 0 && score <= 49) {
-    grade = 'F';
-  } else {
-    grade = 'Invalid score';
-  }
-
-  return grade;
+if (score >= 90 && score <= 100) {
+  grade = 'A';
+} else if (score >= 80 && score <= 89) {
+  grade = 'B';
+} else if (score >= 70 && score <= 79) {
+  grade = 'C';
+} else if (score >= 50 && score <= 69) {
+  grade = 'D';
+} else if (score >= 0 && score <= 49) {
+  grade = 'F';
+} else {
+  grade = 'Invalid score';
 }
