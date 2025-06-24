a0ff7a8b907ac453cef5b4aba6354dde2949a1c8
Sofia Rudik(rufiiss) <fiikiidur@gmail.com>
2025-06-24 22:55:32 +0300

    k9

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,15 +1,19 @@
-let grade;
+function calculateGrade(score) {
+  let grade;
 
-if (score >= 90 && score <= 100) {
-  grade = 'A';
-} else if (score >= 80 && score <= 89) {
-  grade = 'B';
-} else if (score >= 70 && score <= 79) {
-  grade = 'C';
-} else if (score >= 50 && score <= 69) {
-  grade = 'D';
-} else if (score >= 0 && score <= 49) {
-  grade = 'F';
-} else {
-  grade = 'Invalid score';
+  if (score >= 90 && score <= 100) {
+    grade = 'A';
+  } else if (score >= 80 && score <= 89) {
+    grade = 'B';
+  } else if (score >= 70 && score <= 79) {
+    grade = 'C';
+  } else if (score >= 50 && score <= 69) {
+    grade = 'D';
+  } else if (score >= 0 && score <= 49) {
+    grade = 'F';
+  } else {
+    grade = 'Invalid score';
+  }
+
+  return grade;
 }
